d164357695ff32fd9acc36c9ef025348e3ccf544
Liamcr <cariou.liam@orange.fr>
2023-05-30 00:19:27 +0200

    mise en place de l'entité categorie

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,7 +1,8 @@
 <?php
-
 namespace App\Controller;
 
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface; // Importez la classe EntityManagerInterface
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     #[Route('/category/{slug}', name: 'category_show')]
-    public function show(): Response
+    public function show(string $slug, EntityManagerInterface $entityManager): Response
     {
+        $category = $entityManager->getRepository(Category::class)->findOneBy(['slug' => $slug]);
+
+        if (!$category) {
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('category/show.html.twig', [
-            'controller_name' => 'CategoryController',
+            'category' => $category
         ]);
     }
 }
