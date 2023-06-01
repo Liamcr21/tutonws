452c019c1ce3aff448559761ab951f339f8e308b
Liamcr <cariou.liam@orange.fr>
2023-06-02 00:31:18 +0200

    . register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -14,6 +14,7 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
+
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
